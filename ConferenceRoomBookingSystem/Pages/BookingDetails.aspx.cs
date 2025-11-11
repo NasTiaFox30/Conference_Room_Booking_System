95204ad2df204c63a1fcdec9c43ca2885cbbe2dc
Anastasiia Bzova <bzovanastia@gmail.com>
2025-11-11 22:44:22 +0100

    Created Booking.aspx.cs

---
+++ ConferenceRoomBookingSystem/Pages/BookingDetails.aspx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web.UI;
+using ConferenceRoomBookingSystem.Models;
+using ConferenceRoomBookingSystem.Data;
+
+namespace ConferenceRoomBookingSystem.Pages
+{
+    public partial class BookingDetails : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+    }
+}
