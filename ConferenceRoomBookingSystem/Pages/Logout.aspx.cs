8d7c426260fa2e06ef7a93f0c066c40d546da74e
Anastasiia Bzova <bzovanastia@gmail.com>
2025-11-12 19:25:30 +0100

    Delete cookies files

--- ConferenceRoomBookingSystem/Pages/Logout.aspx.cs
+++ ConferenceRoomBookingSystem/Pages/Logout.aspx.cs
@@ -16,6 +16,12 @@ namespace ConferenceRoomBookingSystem.Pages
                 // Clear sesion
                 Session.Clear();
                 Session.Abandon();
+
+                // Delete cookies 
+                if (Response.Cookies["ASP.NET_SessionId"] != null)
+                {
+                    Response.Cookies["ASP.NET_SessionId"].Value = string.Empty;
+                    Response.Cookies["ASP.NET_SessionId"].Expires = DateTime.Now.AddMonths(-20);
         }
             catch (Exception)
             {
