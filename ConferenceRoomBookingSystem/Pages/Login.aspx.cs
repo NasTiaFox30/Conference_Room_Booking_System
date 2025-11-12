8fad18f356faf3ee1aba774b2069450020963fe2
Anastasiia Bzova <bzovanastia@gmail.com>
2025-11-12 17:50:00 +0100

    Page_load added redirect to default page

--- ConferenceRoomBookingSystem/Pages/Login.aspx.cs
+++ ConferenceRoomBookingSystem/Pages/Login.aspx.cs
@@ -6,5 +6,10 @@ namespace ConferenceRoomBookingSystem.Pages
 {
     public partial class Login : Page
     {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack && Session["UserId"] != null)
+                Response.Redirect("~/Pages/Default.aspx");
+        }
     }
 }
