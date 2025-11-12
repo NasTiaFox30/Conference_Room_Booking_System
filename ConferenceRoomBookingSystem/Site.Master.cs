7db81668936ecafdc7b1c1290014450ba85776e0
Anastasiia Bzova <bzovanastia@gmail.com>
2025-11-12 19:43:55 +0100

    Session authentication checking logic

--- ConferenceRoomBookingSystem/Site.Master.cs
+++ ConferenceRoomBookingSystem/Site.Master.cs
@@ -6,6 +6,19 @@ namespace ConferenceRoomBookingSystem
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Session["UserId"] != null)
+            {
+                phAuth.Visible = true;
+                phGuest.Visible = false;
+                lblUserName.Text = Session["FullName"]?.ToString() ?? Session["Username"]?.ToString();
+                AdminLink.Visible = Session["IsAdmin"] != null && (bool)Session["IsAdmin"];
+            }
+            else
+            {
+                phAuth.Visible = false;
+                phGuest.Visible = true;
+                AdminLink.Visible = false;
+            }
         }
     }
 }
